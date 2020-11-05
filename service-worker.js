4c60f10d103150e62e4da6c5e5731b2b2dff76c9
Gin <ginpan@wekids.com.tw>
2020-11-05 15:24:39 +0800

    no message

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ importScripts("/precache-manifest.918f0010891f20c688cbfe5b92cb9636.js", "https:/
 // custom service-worker.js
 if (workbox) {
     // adjust log level for displaying workbox logs
-    workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
+    // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
 
     // apply precaching. In the built version, the precacheManifest will
     // be imported using importScripts (as is workbox itself) and we can 
