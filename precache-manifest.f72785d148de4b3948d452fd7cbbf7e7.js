d2c33bc540b62aab8b9148786f22db4d75dab4bb
Gin <ginpan@wekids.com.tw>
2020-11-11 17:58:46 +0800

    update cam

---
+++ precache-manifest.f72785d148de4b3948d452fd7cbbf7e7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe05fe2661a77450301",
+    "url": "/css/chunk-c7f66dc4.73b17dfc.css"
+  },
+  {
+    "revision": "e4f82024e9a62233d38e",
+    "url": "/css/chunk-vendors.4f1b73d8.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c723c4bf302d33b2622026fdca1e7476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c38dfbf0696a84f48b",
+    "url": "/js/about.33e44a3a.js"
+  },
+  {
+    "revision": "011087ce267cdbeaf2ae",
+    "url": "/js/app.0e2355c6.js"
+  },
+  {
+    "revision": "cfe05fe2661a77450301",
+    "url": "/js/chunk-c7f66dc4.d971d0e5.js"
+  },
+  {
+    "revision": "e4f82024e9a62233d38e",
+    "url": "/js/chunk-vendors.28733b3f.js"
+  },
+  {
+    "revision": "9d2d8052e17d84a7f28e3543c13c3cae",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
